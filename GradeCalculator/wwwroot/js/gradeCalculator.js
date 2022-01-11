7f27efeee99a154106afa4fafd6aca6a26951112
Josh Moody <joshmoody24@gmail.com>
2022-01-10 17:59:21 -0700

    added letter grade functionality

--- GradeCalculator/wwwroot/js/gradeCalculator.js
+++ GradeCalculator/wwwroot/js/gradeCalculator.js
@@ -6,6 +6,21 @@
     "INTEX": .1,
 }
 
+function getLetterGrade(grade) {
+    if (grade >= .94) return "A";
+    else if (grade >= .9) return "A-";
+    else if (grade >= .87) return "B+";
+    else if (grade >= .84) return "B";
+    else if (grade >= .8) return "B-";
+    else if (grade >= .77) return "C+";
+    else if (grade >= .74) return "C";
+    else if (grade >= .7) return "C-";
+    else if (grade >= .67) return "D+";
+    else if (grade >= .64) return "D";
+    else if (grade >= .6) return "D-";
+    else return "E";
+}
+
 $("#calculate").click(function (e) {
     // don't refresh the page when the form submits
     e.preventDefault();
@@ -17,5 +32,5 @@ $("#calculate").click(function (e) {
     gradeTotal += $("#exams").val() * gradeWeights["exams"];
     gradeTotal += $("#intex").val() * gradeWeights["INTEX"];
     // convert answer to percentage and round answer to 2 decimal places
-    $("#result").html(Math.round((gradeTotal * 10000)) / 100 + "%");
+    $("#result").html(Math.round((gradeTotal * 10000)) / 100 + "% (" + getLetterGrade(gradeTotal) + ")");
 });
