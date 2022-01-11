75a0b6d4891c8dfa59d626274a99b722bce9d2b6
Josh Moody <joshmoody24@gmail.com>
2022-01-10 17:46:34 -0700

    added image and various style tweaks

--- GradeCalculator/wwwroot/js/gradeCalculator.js
+++ GradeCalculator/wwwroot/js/gradeCalculator.js
@@ -7,12 +7,15 @@
 }
 
 $("#calculate").click(function (e) {
+    // don't refresh the page when the form submits
     e.preventDefault();
+    // sum up all the grades (with weights)
     let gradeTotal = 0.0;
     gradeTotal += $("#assignments").val() * gradeWeights["assignments"];
     gradeTotal += $("#group-project").val() * gradeWeights["group_project"];
     gradeTotal += $("#quizzes").val() * gradeWeights["quizzes"];
     gradeTotal += $("#exams").val() * gradeWeights["exams"];
     gradeTotal += $("#intex").val() * gradeWeights["INTEX"];
-    $("#result").html(gradeTotal.toPrecision(4) * 100 + "%");
+    // convert answer to percentage and round answer to 2 decimal places
+    $("#result").html(Math.round((gradeTotal * 10000)) / 100 + "%");
 });
